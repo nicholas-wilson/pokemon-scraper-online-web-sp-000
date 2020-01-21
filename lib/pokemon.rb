dc5a30042481d8533f1f97082c00db6af50bbbe8
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-21 02:00:49 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,11 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(name, type, db, id = nil)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
 end
